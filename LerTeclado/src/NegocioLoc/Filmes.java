53bc1d8f99f8170ac27f4567c9a0e20b3803482a
romul <romul@192.168.25.49>
2015-08-27 14:42:09 -0300

    Filmes

---
+++ LerTeclado/src/NegocioLoc/Filmes.java
@@ -0,0 +1,23 @@
+package NegocioLoc;
+
+public class Filmes {
+
+	private String nome;
+	private String tipo;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+}
