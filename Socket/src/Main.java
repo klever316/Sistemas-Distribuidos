6a040722ed548b29a5750232ade82bd423af0fd8
Erick <Erick@10.54.103.23>
2015-09-03 21:05:23 -0300

    Novo projeto Socket

---
+++ Socket/src/Main.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+
+import Cliente.Cliente;
+import Server.Servidor;
+import Server.ServidorThread;
+import Server.TratarConexaoServidorSimples;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		//new Servidor();
+        new Cliente();
+        //new ServidorThread();
+	}
+
+}
